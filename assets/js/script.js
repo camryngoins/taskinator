5e49c59352480f6a245ddbba00be226d53829cf7
camryngoins <camryngoins00@gmail.com>
2022-02-25 18:03:08 -0600

    save tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,7 @@ for (var i = 0; i < tasks.length; i++) {
         tasks[i].name = taskName;
         tasks[i].type = taskType;
     }
+    saveTasks();
 };
 
 alert("Task Updated!");
@@ -75,6 +76,8 @@ var createTaskEl = function(taskDataObj) {
 
      taskDataObj.id = taskIdCounter;
      tasks.push(taskDataObj);
+
+     saveTasks();
 
      var taskActionsEl = createTaskActions(taskIdCounter);
      listItemEl.appendChild(taskActionsEl);
@@ -161,6 +164,7 @@ var deleteTask = function(taskId) {
         // reassign tasks array to be the same as updatedTaskArr
         tasks = updatedTaskArr;
     }
+    saveTasks();
 };
 var editTask = function(taskId) {
     // get task list item element 
@@ -203,6 +207,10 @@ var tasksStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
+    saveTasks();
 };
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 pageContentEl.addEventListener("change", tasksStatusChangeHandler);
